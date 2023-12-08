88c1e2fbd72185f6b6512881f8442aa38c7ab0ab
LenaSaraieva <lena.saraeva1904@gmail.com>
2023-12-08 13:19:29 +0200

    do whike loop 2 tasks

---
+++ script2.js
@@ -0,0 +1,13 @@
+let arr = [3,18,7,-3,12];
+let sum = 0;
+let i = 0;
+
+do {
+    sum = sum + arr[i];
+    i++
+} while 
+    (i < arr.length);
+
+console.log(sum);
+let result = sum / arr.length; // середнє арифметичне 
+console.log(result);
